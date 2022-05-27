6483758c93c8522cbe6032cba86619c15301de89
Максим Братчиков <maxsim.brat4ikov@gmail.com>
2022-05-27 17:12:30 +0400

    Add focus on input translate

--- src/components/TabsTranslateAndCard/Translate/Translator/Translator.jsx
+++ src/components/TabsTranslateAndCard/Translate/Translator/Translator.jsx
@@ -1,22 +1,33 @@
-import React from 'react'
-import { Input, Row, Col } from 'antd'
+import React, { useEffect, useRef } from 'react'
+import {
+  Input, Row, Col,
+} from 'antd'
 
 const { Search } = Input
 
-export const Translator = ({ searchValue, onSetSearchValue, onTranslating = () => {} }) => (
-  <>
-    <Row>
-      <Col span={23}>
-        <Search
-          value={searchValue}
-          onPressEnter={onTranslating}
-          allowClear
-          enterButton="Translate"
-          size="large"
-          onChange={onSetSearchValue}
-          onSearch={onTranslating}
-        />
-      </Col>
-    </Row>
-  </>
-)
+export const Translator = ({ searchValue, onSetSearchValue, onTranslating = () => {} }) => {
+  const inputRef = useRef()
+
+  useEffect(() => {
+    inputRef.current?.focus()
+  }, [])
+
+  return (
+    <>
+      <Row>
+        <Col span={23}>
+          <Search
+            value={searchValue}
+            onPressEnter={onTranslating}
+            allowClear
+            enterButton="Translate"
+            size="large"
+            onChange={onSetSearchValue}
+            onSearch={onTranslating}
+            ref={inputRef}
+          />
+        </Col>
+      </Row>
+    </>
+  )
+}
